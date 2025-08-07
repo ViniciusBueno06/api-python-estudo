449f86378a7cf3d4df586e17d2941a489fce7402
Vinicius Bueno <vini.buenog@gmail.com>
2025-08-07 20:31:38 -0300

    Update teste_apiIBGE.py
    aplicando o rendimento medio

--- teste_apiIBGE.py
+++ teste_apiIBGE.py
@@ -4,7 +4,14 @@ link = "https://servicodados.ibge.gov.br/api/v3/agregados/7832/periodos/202412/v
 requisicao= requests.get(link)
 info= requisicao.json()
 cont = 0
-for item in info:
-    variavel = item.get('resultados')
-    print(variavel)
-    print("=============================================================================================")
+rendMedio=info[3]['resultados']
+print("RENDIMENTO MEDI0 PRODUTOS AGRO BRASIL safra 2024")
+for i in rendMedio:
+    rendProd=i['series'][0]['serie']['202412']
+    nomeProd=list(i['classificacoes'][1]['categoria'].values())[0]
+
+    print("| Produto: "+nomeProd+" |  Rendimento médio: \033[95m"+rendProd+"kg\033[0m")
+    print("\033[92m============================================================\033[0m")
+
+
+
